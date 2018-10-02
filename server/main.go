89858eff46735b7b8a997d94d486f7f848a097ca
Cyle Riggs <beardedfoo@gmail.com>
2018-10-02 23:32:51 +0200

    Send Connection: close header and log cert CN's

--- server/main.go
+++ server/main.go
@@ -5,12 +5,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/http2"
 )
 
 func handler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handler() got %v certificates", len(req.TLS.PeerCertificates))
+	// Request the HTTP client to close this connection, as keeping the connection open provides
+	// a strange UX where the client is not requested to chose a certificate
+	w.Header().Set("Connection", "close")
+
+	cnChain := []string{}
+	for _, cert := range req.TLS.PeerCertificates {
+		cnChain = append(cnChain, string(cert.Subject.CommonName))
+	}
+
+	log.Printf("handler() got %v certificates", strings.Join(cnChain, ","))
+
+
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte(fmt.Sprintf("Received certificates: %v", req.TLS.PeerCertificates)))
 }
