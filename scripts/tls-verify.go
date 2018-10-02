ec6d58fc55a89284aa18909474a69b672ea6bbcf
moredhel <moredhel@aoeu.me>
2018-10-03 00:46:08 +0200

    add tls-verify

---
+++ scripts/tls-verify.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"crypto/x509"
+	"encoding/pem"
+)
+
+func certPEM() []byte {
+	b, err := ioutil.ReadFile("assets/client.crt")
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func rootPEM() []byte {
+	b, err := ioutil.ReadFile("assets/ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func main() {
+	// Verifying with a custom list of root certificates.
+
+	// First, create the set of root certificates. For this example we only
+	// have one. It's also possible to omit this in order to use the
+	// default root set of the current operating system.
+	roots := x509.NewCertPool()
+	ok := roots.AppendCertsFromPEM([]byte(rootPEM()))
+	if !ok {
+		panic("failed to parse root certificate")
+	}
+
+	block, _ := pem.Decode([]byte(certPEM()))
+	if block == nil {
+		panic("failed to parse certificate PEM")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		panic("failed to parse certificate: " + err.Error())
+	}
+
+	opts := x509.VerifyOptions{
+		// DNSName: "beardedfoo",
+		Roots:   roots,
+	}
+
+	if chains, err := cert.Verify(opts); err != nil {
+		fmt.Printf("verification chains: %+v\n", chains)
+		panic("failed to verify certificate: " + err.Error())
+	}
+}
